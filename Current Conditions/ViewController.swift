73dae8347bce4b09a55664a6585f04381c3bd03a
Rosario Tarabocchia <rtarabocchia@gmail.com>
2015-09-29 16:53:05 -0400

    Initial Commit

--- Current Conditions/ViewController.swift
+++ Current Conditions/ViewController.swift
@@ -10,9 +10,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblConditions: UILabel!
+    
+    @IBOutlet weak var txtCity: UITextField!
+    
+    
+    
+    @IBAction func btnGetWeather(sender: AnyObject) {
+        
+        var wasSuccessful = false
+        
+        
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + txtCity.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {  (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let webSiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if webSiteArray!.count > 1 {
+                    
+                    let weatherArray = webSiteArray![1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1 {
+                        
+                        wasSuccessful = true
+                        
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.lblConditions.text = "\(weatherSummary)"
+                        })
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                
+                
+            }
+            
+            if wasSuccessful == false {
+                
+                self.lblConditions.text = "Please enter a valid city."
+            }
+            
+        }
+        
+        
+        task.resume()
+            
+            
+        } else {
+            
+            self.lblConditions.text = "Please enter a valid city."
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
